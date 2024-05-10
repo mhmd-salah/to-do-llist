28bb1313836d08f5ab5664e64a7ac397f5ea1c9d
mhmd <ms.mhmdsalah@gmail.com>
2024-05-10 14:43:36 +0300

    in progress

--- main.js
+++ main.js
@@ -3,8 +3,14 @@ let submit = document.querySelector( ".add" );
 let tasksDiv = document.querySelector( ".tasks" );
 
 //empty array to store the tasks 
-let arrayOfTasks = []
+let arrayOfTasks = [];
+//----trigger function get data from local storage
+getDataFromLocalStorage()
 
+if ( window.localStorage.getItem( "tasks" ) )
+{
+  arrayOfTasks = JSON.parse(window.localStorage.getItem("tasks"))
+} 
 
 // check if input is empty  ----> [1]
 submit.addEventListener( "click", () =>
@@ -16,7 +22,21 @@ submit.addEventListener( "click", () =>
   }
 } );
 
+//click on on task element 
+tasksDiv.addEventListener( "click", ( e ) =>
+{
+  if ( e.target.classList.contains( "del" ) )
+  {
+    //remove task from local storage 
+    deletTaskWith(e.target.parentElement.getAttribute("data-id"))
+    //remove element from page
+    e.target.parentElement.remove()
+  }
+})
 
+
+
+//  add tasks to array from task text input
 function addTaskToArry ( taskText )
 {
   const task = {
@@ -26,5 +46,61 @@ function addTaskToArry ( taskText )
   }
   arrayOfTasks.push(task)
   //add tasks for page
-  addElementToPageFrom(arrayOfTasks)
+  addElementToPageFrom( arrayOfTasks )
+  //add tasks to local storage
+  addDataToLocalStorageFrom( arrayOfTasks )
+}
+
+
+//---------------------------- add element to page from array of tasks
+function addElementToPageFrom ( arrayOfTasks )
+{
+  //empty the tasks div
+  tasksDiv.innerHTML = "";
+  //looping on arry of tasks
+  arrayOfTasks.forEach( ( task ) =>
+  {
+    let div = document.createElement( "div" )
+    div.className = "task"
+    //check if task is done
+    if ( task.completed )
+    {
+      div.className="task done"
+    }
+    div.setAttribute( "data-id", task.id )
+    div.appendChild(document.createTextNode(task.title))
+    let span = document.createElement("span")
+    span.className = "del";
+    span.appendChild(document.createTextNode("delete"))
+    div.appendChild(span)
+    tasksDiv.appendChild( div )
+  })
+}
+
+
+//------------------------
+function addDataToLocalStorageFrom ( arrayOfTasks )
+{
+  window.localStorage.setItem("tasks", JSON.stringify(arrayOfTasks))
+}
+//-------------------------
+
+
+
+
+function getDataFromLocalStorage () // get data from local storage
+{
+  let data = window.localStorage.getItem("tasks")
+  if ( data )
+  {
+    let tasks = JSON.parse(data)
+    addElementToPageFrom(tasks)
+  }
+}
+
+
+function deletTaskWith ( taskId )
+{
+  arrayOfTasks = arrayOfTasks.filter( ( task ) => task.id != taskId )
+  addDataToLocalStorageFrom(arrayOfTasks)
 }
